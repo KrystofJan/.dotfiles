cfd617d5f939674987345ded831e917cca74ce5d
Jan-Krystof Zahradnik <jendazah@gmail.com>
2025-02-03 19:00:37 +0100

    feat: hypridle

--- .config/hypr/start.sh
+++ .config/hypr/start.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 #init wallpaper daemon
+
+hyprlock
+
 swww init &
 
-swww img ~/Wallpapers/gruv-wallpaper.png -o "*" &
+# swww img ~/Wallpapers/gruv-wallpaper.png &
+# swww img ~/Wallpapers/wizard.jpg &
+swww img ~/Downloads/fantasy-portal-moewalls-com.gif &
+swww img ~/Wallpapers/bg.gif --transition-type=center &
+
 
 nm-applet --indicator &
 
