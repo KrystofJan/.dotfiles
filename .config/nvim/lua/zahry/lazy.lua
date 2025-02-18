a8eed55492dd20019e4c80bd6cf0b4fb277b8f93
Jan-Krystof Zahradnik <jan.zahradnik@profiq.com>
2025-02-18 09:48:57 +0100

    fix: lualine

--- .config/nvim/lua/zahry/lazy.lua
+++ .config/nvim/lua/zahry/lazy.lua
@@ -324,33 +324,33 @@ require('lazy').setup({
     end,
   },
 
-{
-  'stevearc/conform.nvim',
-  lazy = false,
-  keys = {
-    {
-      '<leader>f',
-      function()
-        require('conform').format { async = true, lsp_format = "fallback" }
+  {
+    'stevearc/conform.nvim',
+    lazy = false,
+    keys = {
+      {
+        '<leader>f',
+        function()
+          require('conform').format { async = true, lsp_format = 'fallback' }
+        end,
+        mode = '',
+        desc = '[F]ormat buffer',
+      },
+    },
+    opts = {
+      notify_on_error = false,
+      format_on_save = function(bufnr)
+        local disable_filetypes = { c = true, cpp = true }
+        return {
+          timeout_ms = 500,
+          lsp_format = disable_filetypes[vim.bo[bufnr].filetype] and 'none' or 'fallback',
+        }
       end,
-      mode = '',
-      desc = '[F]ormat buffer',
+      formatters_by_ft = {
+        lua = { 'stylua' },
+      },
     },
   },
-  opts = {
-    notify_on_error = false,
-    format_on_save = function(bufnr)
-      local disable_filetypes = { c = true, cpp = true }
-      return {
-        timeout_ms = 500,
-        lsp_format = disable_filetypes[vim.bo[bufnr].filetype] and "none" or "fallback",
-      }
-    end,
-    formatters_by_ft = {
-      lua = { 'stylua' },
-    },
-  },
-},
 
   { -- Autocompletion
     'hrsh7th/nvim-cmp',
